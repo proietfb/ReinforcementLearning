6b708940837b06550015ff36edf5ea709b46d298
proietfb <fabio.proietti.91@gmail.com>
2016-07-28 14:23:29 +0200

    risolto bug algoritmo Q-learning

--- src/QLearning.java
+++ src/QLearning.java
@@ -9,7 +9,7 @@ public class QLearning {
 
     final DecimalFormat df;
     int statesCount;
-    double alpha = 0.1;
+    double alpha = 0.5;
     double gamma = 0.9;
 
     public int[][] R;
@@ -20,30 +20,48 @@ public class QLearning {
         statesCount = agent.getDimGridX() * agent.getDimGridY();
         R = new int[statesCount][statesCount];
         Q = new double[statesCount][statesCount];
-        initRewardValues(agent, node, gridWorld);
+        initRewardValues(node);
         defineQ();
     }
 
-    private void initRewardValues(Agent agent, Node node, GridWorld gridWorld) { //definisce i reward massimi di base nelle posizioni accanto al goal
+    private void initRewardValues(Node node) { //definisce i reward massimi di base nelle posizioni accanto al goal
 
         for (int[] i : R) //inizializzo la matrice dei reward tutti a zero
-            Arrays.fill(i, 1);
+            Arrays.fill(i, 0);
 
-        if (node.getPositionNodeX() == 0)
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX() + 1][node.getPositionNodeY()]] = 100;
-        else if (node.getPositionNodeX() == (agent.getDimGridX() - 1))
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX() - 1][node.getPositionNodeY()]] = 100;
-        else {
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX() - 1][node.getPositionNodeY()]] = 100;
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX() + 1][node.getPositionNodeY()]] = 100;
-        }
-        if (node.getPositionNodeY() == 0)
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY() + 1]] = 100;
-        else if (node.getPositionNodeX() == (agent.getDimGridX() - 1))
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY() - 1]] = 100;
-        else {
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY() + 1]] = 100;
-            R[gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY()]][gridWorld.getGridValues()[node.getPositionNodeX()][node.getPositionNodeY() - 1]] = 100;
+        if (node.getPositionNodeX() == 0 && node.getPositionNodeY() == 0){ //se il punto finale è in 0,0, il reward viene dal basso verso l'alto e da dx verso sx
+            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 100; // dx sx
+            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 100;  //basso alto
+        } else if (node.getPositionNodeX() == 0 && node.getPositionNodeY() == Agent.dimGridY-1) { //punto finale in 0,n il reward viene dal basso verso l'alto e da sx verso dx
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; //sx dx
+            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 100; // basso alto
+        } else if (node.getPositionNodeX() == Agent.dimGridY - 1 && node.getPositionNodeY() == 0){ //punto finale in n,0 reward dall alto verso il basso e dadx verso sx
+            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 100; // alto basso
+            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 100; //dx sx
+        } else if (node.getPositionNodeX() == Agent.dimGridY - 1 && node.getPositionNodeY() == Agent.dimGridY-1) { // punto finale in n,n reward dall alto verso il basso e da sx verso dx
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; // sx dx
+            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 100; // alto basso
+        } else if((node.getPositionNodeX() > 0 && node.getPositionNodeX() < Agent.dimGridX - 1) && node.getPositionNodeY() == 0){ //prima colonna
+            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 100; //alto basso
+            R[node.getNodeCurrentState() + 1 ][node.getNodeCurrentState()] = 100; //dx sx
+            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 100;  //basso alto
+        } else if ((node.getPositionNodeX() > 0 && node.getPositionNodeX() < Agent.dimGridX - 1) && node.getPositionNodeY() == Agent.dimGridY - 1){ //ultima colonna
+            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 100; //alto basso
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; // sx dx
+            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 100;  //basso alto
+        } else if (node.getPositionNodeX() == 0 && (node.getPositionNodeY() > 0 && node.getPositionNodeY() < Agent.dimGridY - 1)) { //prima riga
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; // sx dx
+            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 100; // dx sx
+            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 100;  //basso alto
+        } else if (node.getPositionNodeX() == Agent.dimGridX - 1 && (node.getPositionNodeY() > 0 && node.getPositionNodeY() < Agent.dimGridY - 1)){ //ultima riga
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; // sx dx
+            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 100; // dx sx
+            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 100; //alto basso
+        } else { // in mezzo
+            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 100;  //basso alto
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; // sx dx
+            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 100; // dx sx
+            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 100; //alto basso
         }
 
     }
@@ -66,7 +84,7 @@ public class QLearning {
     }
 
     private int policy(Agent agent, States state) {
-        double maxVal = Double.MIN_VALUE;
+        double maxVal = 0;
         int policyGoToState = agent.getCurrentState();
         for (int i = 0; i < state.definePossibleStates.get(agent.getCurrentState()).size(); i++) {
             int nextState = state.definePossibleStates.get(agent.getCurrentState()).get(i);
@@ -82,39 +100,59 @@ public class QLearning {
     public void run(Agent agent, States states, Node node, GridWorld gridWorld, Actions actions) {
         Random ran = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10000; i++) {
             agent.previousStates.clear();
             agent.setCurrentState(agent.getStartState());
-            System.out.println("init state: " + agent.getCurrentState());
             agent.previousStates.add(agent.getCurrentState());
-            //System.out.println("run" + i);
             int state = agent.getCurrentState();
-            while (state != node.getNodeCurrentState()) {
-                int[] actionsFromState = new int[states.definePossibleStates.get(state).size()];
-                listToArray(actionsFromState, states, state);
-                int index = ran.nextInt(actionsFromState.length);
-                int action = actionsFromState[index];
-                int nextState = action;
-                double q = Q[state][action];
-                double maxQ = maxQ(agent, states);
-                int r = R[state][action];
-                double value = q + alpha * (r + gamma * maxQ - q);
-                setQ(state, action, value);
-                state = nextState;
-                agent.setCurrentState(state);
-                agent.previousStates.add(agent.getCurrentState());
-                //if (i>0)
-                   // actions.possibleActionsFromState(agent, i, gridWorld);
-                //System.out.println( "state: " + state);
-                printWorld(gridWorld);
-                System.out.println("---------------------------");
-            }
 
-            agent.setStartPositionAgentX((int) (0 + Math.random() * (gridWorld.getxGrid() - 1)));
-            agent.setStartPositionAgentY((int) (0 + Math.random() * (gridWorld.getyGrid() - 1)));
-            agent.setStartState(gridWorld.getGridValues()[agent.getStartPositionAgentX()][agent.getStartPositionAgentY()]);
-            agent.setCurrentState(agent.getStartState());
-            //printResult(agent);
+            if (i < 9999) {
+
+                while (state != node.getNodeCurrentState()) {
+                    int[] actionsFromState = new int[states.definePossibleStates.get(state).size()];
+                    listToArray(actionsFromState, states, state);
+                    int index = ran.nextInt(actionsFromState.length);
+                    int action = actionsFromState[index];
+                    int nextState = action;
+                    double q = Q[state][action];
+                    double maxQ = maxQ(agent, states);
+                    int r = R[state][action];
+                    double value = q + alpha * (r + (gamma * maxQ) - q);
+                    setQ(state, action, value);
+                    state = nextState;
+                    agent.setCurrentState(state);
+                    agent.previousStates.add(agent.getCurrentState());
+                }
+
+//                agent.setStartPositionAgentX((int) (0 + Math.random() * (gridWorld.getxGrid() - 1)));
+//                agent.setStartPositionAgentY((int) (0 + Math.random() * (gridWorld.getyGrid() - 1)));
+//                agent.setStartState(gridWorld.getGridValues()[agent.getStartPositionAgentX()][agent.getStartPositionAgentY()]);
+//                agent.setCurrentState(agent.getStartState());
+                //printResult(agent);
+
+            }
+            else {
+                System.out.println(Arrays.deepToString(getQ()));
+                //agent.previousStates.clear();
+                while (state != node.getNodeCurrentState()){
+                    int[] actionFromState = new int[states.definePossibleStates.get(state).size()];
+                    listToArray(actionFromState,states,state);
+                    double maxVal = 0;
+                    int action = actionFromState[ran.nextInt(actionFromState.length)];
+                    for (int j = 0; j < actionFromState.length; j++){
+                        int nextState = actionFromState[j];
+                        double val = Q[state][nextState];
+                        if (val > maxVal){
+                            maxVal = val;
+                            action = nextState;
+                        }
+                    }
+                    state = action;
+                    agent.setCurrentState(state);
+                    agent.previousStates.add(agent.getCurrentState());
+                    //System.out.println(agent.previousStates);
+                }
+            }
 
         }
 
