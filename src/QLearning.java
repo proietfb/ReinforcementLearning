9629e1adf26eea7e7c22ba2942cc1f59582587a9
proietfb <fabio.proietti.91@gmail.com>
2016-08-03 17:35:45 +0200

    Algoritmo Qlearning con policy random+maxQ funzionante

--- src/QLearning.java
+++ src/QLearning.java
@@ -15,54 +15,92 @@ public class QLearning {
     public int[][] R;
     public double[][] Q;
 
-    public QLearning(Agent agent) {
+    public QLearning(Agents agent,Nodes node, Walls walls) {
         df = new DecimalFormat("#.##");
         statesCount = agent.getDimGridX() * agent.getDimGridY();
         R = new int[statesCount][statesCount];
         Q = new double[statesCount][statesCount];
-        //initRewardValues(node);
-        //defineQ();
+        initRewardValues(node,walls);
+        defineQ();
     }
 
-    private void initRewardValues(Node node) { //definisce i reward massimi di base nelle posizioni accanto al goal
+    private void initRewardValues(Nodes node, Walls walls) { //definisce i reward massimi di base nelle posizioni accanto al goal
 
         for (int[] i : R) //inizializzo la matrice dei reward tutti a zero
             Arrays.fill(i, 0);
 
         if (node.getPositionNodeX() == 0 && node.getPositionNodeY() == 0){ //se il punto finale è in 0,0, il reward viene dal basso verso l'alto e da dx verso sx
             R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 10; // dx sx
-            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
-        } else if (node.getPositionNodeX() == 0 && node.getPositionNodeY() == Agent.dimGridY-1) { //punto finale in 0,n il reward viene dal basso verso l'alto e da sx verso dx
-            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 100; //sx dx
-            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 10; // basso alto
-        } else if (node.getPositionNodeX() == Agent.dimGridY - 1 && node.getPositionNodeY() == 0){ //punto finale in n,0 reward dall alto verso il basso e dadx verso sx
-            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 10; // alto basso
+            R[node.getNodeCurrentState() + Agents.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
+        } else if (node.getPositionNodeX() == 0 && node.getPositionNodeY() == Agents.dimGridY-1) { //punto finale in 0,n il reward viene dal basso verso l'alto e da sx verso dx
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; //sx dx
+            R[node.getNodeCurrentState() + Agents.dimGridX][node.getNodeCurrentState()] = 10; // basso alto
+        } else if (node.getPositionNodeX() == Agents.dimGridY - 1 && node.getPositionNodeY() == 0){ //punto finale in n,0 reward dall alto verso il basso e dadx verso sx
+            R[node.getNodeCurrentState() - Agents.dimGridX][node.getNodeCurrentState()] = 10; // alto basso
             R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 10; //dx sx
-        } else if (node.getPositionNodeX() == Agent.dimGridY - 1 && node.getPositionNodeY() == Agent.dimGridY-1) { // punto finale in n,n reward dall alto verso il basso e da sx verso dx
+        } else if (node.getPositionNodeX() == Agents.dimGridY - 1 && node.getPositionNodeY() == Agents.dimGridY-1) { // punto finale in n,n reward dall alto verso il basso e da sx verso dx
             R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; // sx dx
-            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 10; // alto basso
-        } else if((node.getPositionNodeX() > 0 && node.getPositionNodeX() < Agent.dimGridX - 1) && node.getPositionNodeY() == 0){ //prima colonna
-            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
+            R[node.getNodeCurrentState() - Agents.dimGridX][node.getNodeCurrentState()] = 10; // alto basso
+        } else if((node.getPositionNodeX() > 0 && node.getPositionNodeX() < Agents.dimGridX - 1) && node.getPositionNodeY() == 0){ //prima colonna
+            R[node.getNodeCurrentState() - Agents.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
             R[node.getNodeCurrentState() + 1 ][node.getNodeCurrentState()] = 10; //dx sx
-            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
-        } else if ((node.getPositionNodeX() > 0 && node.getPositionNodeX() < Agent.dimGridX - 1) && node.getPositionNodeY() == Agent.dimGridY - 1){ //ultima colonna
-            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
+            R[node.getNodeCurrentState() + Agents.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
+        } else if ((node.getPositionNodeX() > 0 && node.getPositionNodeX() < Agents.dimGridX - 1) && node.getPositionNodeY() == Agents.dimGridY - 1){ //ultima colonna
+            R[node.getNodeCurrentState() - Agents.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
             R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; // sx dx
-            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
-        } else if (node.getPositionNodeX() == 0 && (node.getPositionNodeY() > 0 && node.getPositionNodeY() < Agent.dimGridY - 1)) { //prima riga
-            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; // sx dx
-            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 10; // dx sx
-            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
-        } else if (node.getPositionNodeX() == Agent.dimGridX - 1 && (node.getPositionNodeY() > 0 && node.getPositionNodeY() < Agent.dimGridY - 1)){ //ultima riga
+            R[node.getNodeCurrentState() + Agents.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
+        } else if (node.getPositionNodeX() == 0 && (node.getPositionNodeY() > 0 && node.getPositionNodeY() < Agents.dimGridY - 1)) { //prima riga
             R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; // sx dx
             R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 10; // dx sx
-            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
+            R[node.getNodeCurrentState() + Agents.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
+        } else if (node.getPositionNodeX() == Agents.dimGridX - 1 && (node.getPositionNodeY() > 0 && node.getPositionNodeY() < Agents.dimGridY - 1)){ //ultima riga
+            R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; // sx dx
+            R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 10; // dx sx
+            R[node.getNodeCurrentState() - Agents.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
         } else { // in mezzo
-            R[node.getNodeCurrentState() + Agent.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
+            R[node.getNodeCurrentState() + Agents.dimGridX][node.getNodeCurrentState()] = 10;  //basso alto
             R[node.getNodeCurrentState() - 1][node.getNodeCurrentState()] = 10; // sx dx
             R[node.getNodeCurrentState() + 1][node.getNodeCurrentState()] = 10; // dx sx
-            R[node.getNodeCurrentState() - Agent.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
+            R[node.getNodeCurrentState() - Agents.dimGridX][node.getNodeCurrentState()] = 10; //alto basso
         }
+
+        for(int i = 0; i< walls.nOfWalls;i++){
+            if (walls.getWallsStatesPositions()[i] == 0){ //0,0
+                R[walls.getWallsStatesPositions()[i] + 1][walls.getWallsStatesPositions()[i]] = -1; // dx sx
+                R[walls.getWallsStatesPositions()[i] + Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; //basso alto
+            } else if (walls.getWallsStatesPositions()[i] == ((1*Agents.dimGridY)-1)) { //0,n
+                R[walls.getWallsStatesPositions()[i] - 1][walls.getWallsStatesPositions()[i]] = -1; //sx dx
+                R[walls.getWallsStatesPositions()[i] + Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // basso alto
+            } else if (walls.getWallsStatesPositions()[i] == ((Agents.dimGridX*Agents.dimGridY)-Agents.dimGridX)) { //n,0
+                R[walls.getWallsStatesPositions()[i] + 1][walls.getWallsStatesPositions()[i]] = -1; // dx sx
+                R[walls.getWallsStatesPositions()[i] - Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // alto basso
+            } else if (walls.getWallsStatesPositions()[i] == ((Agents.dimGridX*Agents.dimGridY) - 1)) { //n,n
+                R[walls.getWallsStatesPositions()[i] - 1][walls.getWallsStatesPositions()[i]] = -1; //sx dx
+                R[walls.getWallsStatesPositions()[i] - Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // alto basso
+            } else if ((walls.wallPositionX[i] > 0 && walls.getWallPositionX()[i] < Agents.dimGridX - 1) && walls.getWallPositionY()[i] == 0) { //prima colonna
+                R[walls.getWallsStatesPositions()[i] - Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // alto basso
+                R[walls.getWallsStatesPositions()[i] + 1][walls.getWallsStatesPositions()[i]] = -1; // dx sx
+                R[walls.getWallsStatesPositions()[i] + Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // basso alto
+            } else if ((walls.wallPositionX[i] > 0 && walls.wallPositionX[i] < Agents.dimGridX - 1) && walls.wallPositionY[i] == Agents.dimGridY - 1){ //ultima colonna
+                R[walls.getWallsStatesPositions()[i] - Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // alto basso
+                R[walls.getWallsStatesPositions()[i] - 1][walls.getWallsStatesPositions()[i]] = -1; //sx dx
+                R[walls.getWallsStatesPositions()[i] + Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // basso alto
+            } else if (walls.getWallPositionX()[i] == 0 && (walls.getWallPositionY()[i] > 0 && walls.getWallPositionY()[i]< Agents.dimGridY - 1)){ //prima riga
+                R[walls.getWallsStatesPositions()[i] - 1][walls.getWallsStatesPositions()[i]] = -1; //sx dx
+                R[walls.getWallsStatesPositions()[i] + 1][walls.getWallsStatesPositions()[i]] = -1; // dx sx
+                R[walls.getWallsStatesPositions()[i] + Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // basso alto
+            } else if (walls.getWallPositionX()[i] == Agents.dimGridX - 1 && (walls.getWallPositionY()[i] > 0 && walls.getWallPositionY()[i] < Agents.dimGridY - 1)) { //ultima riga
+                R[walls.getWallsStatesPositions()[i] - 1][walls.getWallsStatesPositions()[i]] = -1; //sx dx
+                R[walls.getWallsStatesPositions()[i] + 1][walls.getWallsStatesPositions()[i]] = -1; // dx sx
+                R[walls.getWallsStatesPositions()[i] - Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // alto basso
+            } else {
+                R[walls.getWallsStatesPositions()[i] + Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // basso alto
+                R[walls.getWallsStatesPositions()[i] - 1][walls.getWallsStatesPositions()[i]] = -1; //sx dx
+                R[walls.getWallsStatesPositions()[i] + 1][walls.getWallsStatesPositions()[i]] = -1; // dx sx
+                R[walls.getWallsStatesPositions()[i] - Agents.dimGridX][walls.getWallsStatesPositions()[i]] = -1; // alto basso
+            }
+        }
+
 
     }
 
@@ -85,7 +123,7 @@ public class QLearning {
         return maxVal;
     }
 
-    private int policy(Agent agent, States state, int[] array) {
+    private int policy(Agents agent, States state, int[] array) {
         Random ran = new Random();
         double maxVal = 0;
         int policyGoToState = agent.getCurrentState();
@@ -107,13 +145,9 @@ public class QLearning {
             return policyGoToState;
     }
 
-    public void run(Agent agent, States states, Node node, GridWorld gridWorld) {
+    public void run(Agents agent, States states, Nodes node, GridWorld gridWorld) {
         Random ran = new Random();
-        initRewardValues(node);
-        defineQ();
         for (int i = 0; i < 100; i++) {
-            agent.previousStates.clear();
-            agent.setCurrentState(agent.getStartState());
             agent.previousStates.add(agent.getCurrentState());
             int state = agent.getCurrentState();
 
@@ -122,9 +156,6 @@ public class QLearning {
                 while (state != node.getNodeCurrentState()) {
                     int[] actionsFromState = new int[states.definePossibleStates.get(state).size()];
                     listToArray(actionsFromState, states, state);
-//                    int action = policy(agent,states,actionsFromState);
-//                    int nextState = action;
-//                    double q =  Q[state][action];
                     int index = ran.nextInt(actionsFromState.length);
                     int action = actionsFromState[index];
                     int nextState = action;
@@ -137,17 +168,14 @@ public class QLearning {
                     agent.setCurrentState(state);
                     agent.previousStates.add(agent.getCurrentState());
                 }
-
-                agent.setStartPositionAgentX((int) (0 + Math.random() * (gridWorld.getxGrid() - 1)));
-                agent.setStartPositionAgentY((int) (0 + Math.random() * (gridWorld.getyGrid() - 1)));
-                agent.setStartState(gridWorld.getGridValues()[agent.getStartPositionAgentX()][agent.getStartPositionAgentY()]);
-                agent.setCurrentState(agent.getStartState());
                 //printResult();
+                agent.previousStates.clear();
+                gridWorld.fillGridWorldAgents(agent); //ad ogni ciclo ogni agente parte da uno stato iniziale random
+
 
             }
             else {
-                printResult();
-                //agent.previousStates.clear();
+                //printResult();
                 while (state != node.getNodeCurrentState()){
                     int[] actionFromState = new int[states.definePossibleStates.get(state).size()];
                     listToArray(actionFromState,states,state);
