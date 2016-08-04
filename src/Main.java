90168805222f4ae620d2b6195ef2fa60048a5075
proietfb <fabio.proietti.91@gmail.com>
2016-08-04 16:56:53 +0200

    Problemi nella gestione di obiettivi e agenti multipli

--- src/Main.java
+++ src/Main.java
@@ -5,54 +5,71 @@ import java.util.Arrays;
  */
 public class Main {
 
+    static int x,y,nOfWalls,nAgents, nNodes;
 
-    public Main() {
-    }
+    public Main() {}
 
     public static void main(String[] args) {
+
         long BEGIN = System.currentTimeMillis();
-        int x = 7, y = 7, nOfWalls = 6;
+
+        x = 7;
+        y = 7;
+        nOfWalls = 6;
+        nAgents = 2;
+        nNodes = 2;
 
         GridWorld newWorld = new GridWorld(x, y);
         Walls walls = new Walls(nOfWalls);
-        Agents agent0 = new Agents(x, y);
-        Nodes node0 = new Nodes();
+        Agents[] agents = new Agents[nAgents];
+        Nodes[] nodes = new Nodes[nNodes];
         States state = new States();
-        Actions action = new Actions();
 
         newWorld.defineWorld();
         newWorld.defineGridValues();
         state.defineStates(newWorld);
         newWorld.fillGridWorldWalls(x,y,walls);
-        newWorld.fillGridWorldAgents(agent0);
-        newWorld.fillGridWorldNodes(node0);
-        //agent0.setCurrentPositionX(agent0.getStartPositionAgentX());
-        //agent0.setCurrentPositionY(agent0.getStartPositionAgentY());
 
-        //agent0.currentState(newWorld);
+        for (int i = 0; i < nodes.length; i++){
+            nodes[i] = new Nodes();
+            newWorld.fillGridWorldNodes(nodes[i]);
+            System.out.println("state of node " + i + ": "+ nodes[i].getNodeCurrentState());
 
-        QLearning q = new QLearning(agent0, node0, walls);
+        }
 
 
-        q.run(agent0, state, node0, newWorld);
+        for (int i = 0; i < agents.length; i++){
+            agents[i] = new Agents(x,y);
+            newWorld.fillGridWorldAgents(agents[i]);
+            System.out.println("state of agent " + i + ": "+ agents[i].getCurrentState());
+        }
+
+        for (int i = 0; i < agents.length;i++){
+            for (int j=0; j< nodes.length;j++){
+                agents[i].nodesStatesPositions[j] = nodes[j].getNodeCurrentState();
+            }
+        }
+        System.out.println(Arrays.toString(agents[0].getNodesStatesPositions()));
+        System.out.println(Arrays.toString(agents[1].getNodesStatesPositions()));
+        for (int i = 0; i < agents.length;i++){
+            QLearning q = new QLearning(agents[i], nodes[i], walls);
+            q.runMultiAgent(agents[i], state, nodes, newWorld);
+        }
+
+
 
         System.out.println("walls: " + Arrays.toString(walls.getWallsStatesPositions()));
         System.out.println(Arrays.deepToString(newWorld.getGridValues()));
         printWorld(newWorld);
         System.out.println("\n");
-        //printMatrix(q);
-        System.out.println(agent0.getPreviousStates());
-
-        //printQ(q);
-        //q.printResult(agent0);
+        System.out.println("agent 0: " + agents[0].getPreviousStates());
+        System.out.println("agent 1: " +agents[1].getPreviousStates());
 
         long END = System.currentTimeMillis();
         System.out.println("Time: " + (END - BEGIN) / 1000.0 + " sec.");
-
-
     }
 
-    public static void printWorld(GridWorld gridWorld) {
+    private static void printWorld(GridWorld gridWorld) {
         for (int i = 0; i < gridWorld.getGridW().length; i++) {
             for (int j = 0; j < gridWorld.getGridW()[0].length; j++) {
                 System.out.print(gridWorld.getGridW()[i][j] + " ");
@@ -61,7 +78,7 @@ public class Main {
         }
     }
 
-    public static void printQ(QLearning qLearning) {
+    private static void printQ(QLearning qLearning) {
         for (int i = 0; i < qLearning.getQ().length; i++) {
             for (int j = 0; j < qLearning.getQ()[0].length; j++) {
                 System.out.print(qLearning.getQ()[i][j] + " ");
@@ -70,14 +87,13 @@ public class Main {
         }
     }
 
-    public static void printR(QLearning q) {
-        for (int i = 0; i < q.R.length; i++) {
-            for (int j = 0; j < q.R[0].length; j++) {
-                System.out.print(q.R[i][j] + " ");
+    private static void printR(QLearning q) {
+        for (int i = 0; i < q.getR().length; i++) {
+            for (int j = 0; j < q.getR()[0].length; j++) {
+                System.out.print(q.getR()[i][j] + " ");
             }
             System.out.print("\n");
         }
     }
-
 
 }
